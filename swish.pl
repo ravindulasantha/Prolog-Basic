7615085f3673931074d100ad8dbe9699173bfd1c
Ravindu Lasantha <67038119+ravindulasantha@users.noreply.github.com>
2023-09-09 16:25:18 +0530

    Create swish.pl

---
+++ swish.pl
@@ -0,0 +1,17 @@
+country(sri-lanka).
+country('Sri Lanka').
+country(india).
+
+capital(sri-lanka, sjp).
+capital(india, new-delgi).
+capital(pakistan, islambad).
+capital(singapore, singapore).
+capital(bangladesh, dhaka).
+capital(nepal, kathmandu).
+
+likes(john, mary).
+likes(john, ravi).
+likes(john, pamela).
+likes(mary, ravi).
+likes(pamela, john).
+
